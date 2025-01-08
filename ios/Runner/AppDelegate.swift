a74fe3a631c42dd5229703ee499417420182aa62
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2025-01-08 17:23:31 +0600

    method channel example

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import WidgetKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,23 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "nutrition_widget", binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if call.method == "updateWidget" {
+          if #available(iOS 14.0, *) {
+              WidgetCenter.shared.reloadAllTimelines()
+              result(nil)
+          } else {
+              result(FlutterMethodNotImplemented)
+          }
+      } else {
+          result(FlutterMethodNotImplemented)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
